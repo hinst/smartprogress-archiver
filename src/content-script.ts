0720fc4faa5bf3f6687f8dd3880de35ca15179fe
Alexander Savinykh <asavinykh@nbrx.de>
2021-01-25 23:13:01 +0100

    content-script: look at FEED_LIST of SmartProgress

--- src/content-script.ts
+++ src/content-script.ts
@@ -2,8 +2,13 @@ import { MSG_EXPORT } from "./common";
 
 console.log('CONTENT SCRIPT STARTED');
 
+function startArchiving() {
+    const feedList = window['wrappedJSObject'].FEED_LIST;
+    console.log(feedList);
+}
+
 browser.runtime.onMessage.addListener((message: any) => {
     if (message == MSG_EXPORT) {
-        console.log('e');
+        startArchiving();
     }
 });
