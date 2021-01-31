5262b0e45fc93d9460e2ecd70dcc0e9443509b80
hinst <hinstance@yandex.ru>
2021-01-31 23:40:44 +0100

    fixed build-package script

--- src/build-package.ts
+++ src/build-package.ts
@@ -2,4 +2,9 @@ import * as fs from 'fs';
 import * as child_process from 'child_process';
 import * as rimraf from 'rimraf';
 
-child_process.execSync(`node_modules\\.bin\\web-ext sign --api-key=${}`);
+const apiKey = fs.readFileSync('./moz-key.txt');
+const apiSecret = fs.readFileSync('./moz-key-secret.txt');
+const command = 'node_modules\\.bin\\web-ext sign ' +
+    '--source-dir=dist-ext ' +
+    `--api-key=${apiKey} --api-secret=${apiSecret}`;
+child_process.execSync(command, {stdio: 'inherit'});
